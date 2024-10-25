12a7925626ad21965894f863266bff90b094262d
helwyza <helwyza@gmail.com>
2024-10-25 22:16:30 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "pulse-fast": "pulse 0.5s linear infinite",
+        "ping-slow": "ping 3s linear infinite",
+        "bounce-fast": "bounce 0.5s linear infinite",
+      },
+      screens: {
+        lg: "1000px",
+        "2xl": "1000px",
+      },
+    },
+  },
+  plugins: [],
+};
